823ee3b2a5285de06d0faf041e206707158c5148
Imambashar <85121070+Imambashar@users.noreply.github.com>
2024-10-13 16:59:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/patientRecords", patientRecordRoutes);
 
 // bundle frontend code here
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
